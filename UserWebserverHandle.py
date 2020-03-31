bfeea9cc6e28ae89e11251ec78cc2b0404b00d48
Amit Kumar <amitkr0819@gmail.com>
2020-03-31 20:13:15 +0530

    updating manager

--- UserWebserverHandle.py
+++ UserWebserverHandle.py
@@ -62,31 +62,29 @@ def hotel_sms_reply():
     elif msg == 'c':
         append_menu_msg="To update menu of your restaurant\nGive instruction as:\n hotel no. menu <name of the item> <price> <password>\nfor instance if you want to update menu of Sepoy Grande give this instruction\n1 menu roti 30 ****"
         resp.message(append_menu_msg)
-    elif len(msg)>10 and int(msg.split()[0])<7 :
-        # len=len(msg.split())
-        try :
-            if msg.split()[1] == 'bookings':
-                showBooking,success=databaseHandle.showBookings(msg)
-                if success:
-                    resp.message(showBooking)
-                else :
-                    error_msg="Either the instruction is invalid or password\nPlease try again"
-                    resp.message(error_msg)
-            elif msg.split()[1] == 'table':
-                seatstr,success=databaseHandle.updateSeats(msg)
-                if success:
-                    resp.message("No. of available tables have been successfully updated for "+restaurant[int(msg[0])]+"\n"+seatstr)
-                else :
-                    error_msg="Either the instruction or password is invalid\nPlease try again !"
-                    resp.message(error_msg)
-            elif msg.split()[1] == 'menu':
-                menuStr,success = databaseHandle.updateMenu(msg,len)
-                if success:
-                    resp.message("Menu have been successfully updated for "+restaurant[int(msg[0])]+"\n"+menuStr)
-                else:
-                    error_msg = "Either the instruction is invalid or password\nPlease try again"
-                    resp.message(error_msg)
-        except:
+    elif len(msg)>10 and len(msg.split())>2 and int(msg.split()[0])<7:
+        if msg.split()[1] == 'bookings':
+            showBooking,success=databaseHandle.showBookings(msg)
+            if success:
+                resp.message(showBooking)
+            else :
+                error_msg="Either the instruction is invalid or password\nPlease try again"
+                resp.message(error_msg)
+        elif msg.split()[1] == 'table':
+            seatstr,success=databaseHandle.updateSeats(msg)
+            if success:
+                resp.message("No. of available tables have been successfully updated for "+restaurant[int(msg[0])-1]+"\n"+seatstr)
+            else :
+                error_msg="Either the instruction or password is invalid\nPlease try again !"
+                resp.message(error_msg)
+        elif msg.split()[1] == 'menu':
+            menuStr,success = databaseHandle.updateMenu(msg,len)
+            if success:
+                resp.message("Menu have been successfully updated for "+restaurant[int(msg[0])-1]+"\n"+menuStr)
+            else:
+                error_msg = "Either the instruction is invalid or password\nPlease try again"
+                resp.message(error_msg)
+        else:
             error_msg = "Invalid instruction!\nTry giving 'Dine out'"
             resp.message(error_msg)
     else:
